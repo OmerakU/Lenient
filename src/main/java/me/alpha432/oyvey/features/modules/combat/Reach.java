4d1eeddc3f4de27a77bfb350d9a0f1068d18aaa4
OmerakU <omerakuyt@gmail.com>
2025-07-05 22:05:33 +0800

    Update Reach.java

--- src/main/java/me/alpha432/oyvey/features/modules/combat/Reach.java
+++ src/main/java/me/alpha432/oyvey/features/modules/combat/Reach.java
@@ -2,45 +2,26 @@ package me.alpha432.oyvey.features.modules.combat;
 
 import me.alpha432.oyvey.features.modules.Module;
 import me.alpha432.oyvey.features.setting.Setting;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.util.math.Box;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.client.MinecraftClient;
 
-public class HitboxExtender extends Module {
-    public Setting<Double> extendSize = this.register(new Setting<>("ExtendSize", 0.5, 0.1, 2.0));
+public class ReachExtender extends Module {
     public Setting<Double> reach = this.register(new Setting<>("Reach", 4.5, 3.0, 10.0));
 
-    public HitboxExtender() {
-        super("HitboxExtend", "Тоглогчдын Hitbox болон Reach-ийг өргөтгөнө.", Category.COMBAT, true, false, false);
+    public ReachExtender() {
+        super("ReachExtender", "Цохилтын хүрээг өргөтгөнө.", Category.COMBAT, true, false, false);
     }
 
-    @Override
-    public void onRender3D(MatrixStack matrixStack, VertexConsumerProvider vertexConsumers) {
-        MinecraftClient mc = MinecraftClient.getInstance();
-
-        for (Entity entity : mc.world.getEntities()) {
-            if (entity instanceof LivingEntity && entity != mc.player) {
-                Box hitbox = entity.getBoundingBox();
-                double size = extendSize.getValue();
-                Box extended = new Box(
-                    hitbox.minX - size,
-                    hitbox.minY,
-                    hitbox.minZ - size,
-                    hitbox.maxX + size,
-                    hitbox.maxY + size,
-                    hitbox.maxZ + size
-                );
-                WorldRenderer.drawBox(matrixStack, vertexConsumers.getBuffer(WorldRenderer.getOutlineLayer()), extended, 1f, 0f, 0f, 0.3f);
-            }
-        }
-    }
-
-    // This method would be used somewhere to get the current reach distance
+    // Call this method when calculating attack reach, example in attack logic
     public double getReach() {
         return reach.getValue();
+    }
+    
+    // Example usage: replace vanilla reach check in your attack handling with this method
+    public boolean canReachEntity(PlayerEntity player, PlayerEntity target) {
+        double distSq = player.squaredDistanceTo(target);
+        double maxReach = getReach();
+        return distSq <= maxReach * maxReach;
     }
 }
