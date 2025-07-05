9e1a11b220a1ed3180760e97f8413f6d4e416888
OmerakU <omerakuyt@gmail.com>
2025-07-05 21:56:24 +0800

    Update MCF.java

--- src/main/java/me/alpha432/oyvey/features/modules/misc/MCF.java
+++ src/main/java/me/alpha432/oyvey/features/modules/misc/MCF.java
@@ -11,7 +11,7 @@ import org.lwjgl.glfw.GLFW;
 public class MCF extends Module {
     private boolean pressed;
     public MCF() {
-        super("MCF", "Middle click friend", Category.MISC, true, false, false);
+        super("Дунд товч Найз", "Тоглогч дээр дунд товч дарснаар найздаа/ally нэмнэ.", Category.MISC, true, false, false);
     }
 
     @Override public void onTick() {
